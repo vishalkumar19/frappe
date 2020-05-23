9445de9a2bba38d8dc1060f7d73160e39ce29851
Michelle Alva <50285544+michellealva@users.noreply.github.com>
2020-05-23 08:46:35 +0530

    fix: fix error message
    fixed typo in error message

--- frappe/email/doctype/email_account/email_account.py
+++ frappe/email/doctype/email_account/email_account.py
@@ -347,7 +347,7 @@ class EmailAccount(Document):
 			# and we don't want emails sent by us to be pulled back into the system again
 			# dont count emails sent by the system get those
 			if frappe.flags.in_test:
-				print('WARN: Cannot pull email. Sender sames as recipient inbox')
+				print('WARN: Cannot pull email. Sender same as recipient inbox')
 			raise SentEmailInInbox
 
 		if email.message_id:
