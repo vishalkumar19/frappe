c04c46c1da074c651c693bbe80809532886239b8
VISHAL KUMAR <54323540+vishalkumar19@users.noreply.github.com>
2020-08-03 02:21:44 +0530

    commit

--- frappe/config/desktop.py
+++ frappe/config/desktop.py
@@ -6,16 +6,6 @@ def get_data():
 	return [
 		# Administration
 		{
-			"module_name": "Inventory management",
-			"category": "Administration",
-			"label": _("Tools"),
-			"color": "#FFF5A7",
-			"reverse": 1,
-			"icon": "octicon octicon-calendar",
-			"type": "module",
-			"description": "Todos, notes, calendar and newsletter."
-		},
-		{
 			"module_name": "Desk",
 			"category": "Administration",
 			"label": _("Tools"),
